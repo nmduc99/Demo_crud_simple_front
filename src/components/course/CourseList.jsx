6688df260f4f647c1a21da30e0966f52d45ae484
unknown <you@example.com>
2021-03-29 11:12:18 +0700

    update29.3

--- src/components/course/CourseList.jsx
+++ src/components/course/CourseList.jsx
@@ -1,22 +1,56 @@
 import { useEffect, useState } from "react";
-//import axios from 'axios'
-
-import { Button } from 'reactstrap';
+import Axios from 'axios'
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter} from 'reactstrap';
 import { Table } from 'reactstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 function CourseList() {
-    const [courses, setCourse] = useState([]);
+    const deleteCourse = async (id) => {
+        try {
+            await Axios.delete(`http://localhost:8080/courses/${id}`);
+            await getAll();
+        } catch (error) {
+            console.log(error);
+        }
+    }
 
+
+    async function getAll() {
+        setState({ loading: true, data: [] });
+        const response = await Axios.get(`http://localhost:8080/courses`);
+        setState({ loading: false, data: response.data || [] });
+    }
+
+
+    async function handleOnSubmit(e) {
+        e.preventDefault();
+        const data = {
+            code,
+            name,
+            descrition
+        }
+        console.log(data)
+        setModal(false);
+        await Axios.post('http://localhost:8080/courses', data);
+        getAll();
+    }
+    function handleOnAdd(){
+        setModal(true);
+    }
+    function handleCancel(){
+        setModal(false);
+    }
+    
+    const [code, setCode] = useState();
+    const [name, setName] = useState();
+    const [descrition, setDescrition] = useState();
+    const [modal, setModal] = useState(false);
+
+
+    const [state, setState] = useState({ loading: true, data: [] });
+    const { loading, data } = state || {};
+    console.log('render', state);
     useEffect(() => {
-        // async function getAll() {
-        //     const response = await axios.get(`http://localhost:8080/courses`)
-        //         .then(res => res.data)
-        //     setCourse(response);
-        // }
-        // getAll()
-        fetch('http://localhost:8080/courses')
-        .then(response => response.json())
-        .then(data => setCourse(data));
+        getAll();
     }, [])
 
 
@@ -24,37 +58,77 @@ function CourseList() {
         <div>
             <h1>Course list</h1>
             <p> </p>
-            <Button color="primary" >Add</Button>
+            <Button color="primary" onClick={handleOnAdd}>Add</Button>
             <p> </p>
-            <Table bordered>
-                <thead>
-                    <tr>
-                        <td><b>Id</b></td>
-                        <td><b>Code</b></td>
-                        <td><b>Name</b></td>
-                        <td><b>Descrition</b></td>
-                        <td></td>
-                        <td></td>
-                    </tr>
-                </thead>
-                <tbody>
-                    {
-                        courses && courses.map(courses => (
-                            <tr>
-                                <td>{courses.id}</td>
-                                <td>{courses.code}</td>
-                                <td>{courses.name}</td>
-                                <td>{courses.descrition}</td>
-                                <td><Button color="info">Edit</Button>{' '}
-                                    <Button color="danger">Delete</Button>
-                                </td>
+            {!loading && (
+                <Table bordered>
+                    <thead>
+                        <tr>
+                            <td><b>Id</b></td>
+                            <td><b>Code</b></td>
+                            <td><b>Name</b></td>
+                            <td><b>Descrition</b></td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            data && data.map((item, courseIndex) => (
+                                <tr key={courseIndex}>
+                                    <td>{item.id}</td>
+                                    <td>{item.code}</td>
+                                    <td>{item.name}</td>
+                                    <td>{item.descrition}</td>
+                                    <td><Button color="info">Edit</Button>{' '}
+                                        <Button onClick={() => {
+                                            deleteCourse(item.id);
+                                        }}
+                                            color="danger">Delete</Button>
+                                    </td>
 
-                            </tr>
-                        ))
-                    }
-                </tbody>
+                                </tr>
+                            ))
+                        }
+                    </tbody>
 
-            </Table>
+                </Table>
+            )}
+
+            <div>
+
+                <Modal isOpen={modal} fade={true}   >
+                    <ModalHeader >Add Student</ModalHeader>
+                    <ModalBody>
+                        <div>
+                            <form id="formSubmit" onSubmit={handleOnSubmit} >
+                            <div>
+                                    <label for="code">Code:</label><br />
+                                    <input type="text" id="code" name="code"
+                                        value={code} onChange={e => setCode(e.target.value)} /> <br />
+                                </div>
+                                <div>
+                                    <label for="name">Name:</label> <br />
+                                    <input type="text" id="name" name="name"
+                                        value={name} onChange={e => setName(e.target.value)} /> <br />
+                                </div>
+                              
+                                <div>
+                                    <label for="descrition">Descrition:</label> <br />
+                                    <input type="text" id="descrition" name="descrition"
+                                        value={descrition} onChange={e =>  setDescrition(e.target.value)} /> <br />
+                                </div>
+                               
+
+                            </form>
+                        </div>
+                    </ModalBody>
+                    <ModalFooter>
+                        <Button color="primary" type="submit" form="formSubmit" >Save</Button>{' '}
+                        <Button color="secondary" onClick={handleCancel}>Cancel</Button>
+                    </ModalFooter>
+                </Modal>
+            </div>
         </div>
 
     )
--- src/components/student/StudentList.jsx
+++ src/components/student/StudentList.jsx
@@ -1,77 +1,149 @@
 import { useEffect, useState } from "react";
 import React from "react"
 import Axios from 'axios'
-import { Button } from 'reactstrap';
-import { Table } from 'reactstrap';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter, Table } from 'reactstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from "axios";
 
 function StudentList() {
-    // const [state, setState] = useState([])
-    const deleteTask = (id)=> {
-        Axios({
-            method: "DELETE",
-            url: `http://localhost:8080/students/${id}`,
-          }).catch((err) => {
-            console.log(err);
-          });
+
+    const deleteStudent = async (id) => {
+        try {
+            await Axios.delete(`http://localhost:8080/students/${id}`);
+            await getAll();
+        } catch (error) {
+            console.log(error);
+        }
     }
-    const [students, setStudent] = useState([]);
+
+
+
+    async function getAll() {
+        setState({ loading: true, data: [] });
+        const response = await Axios.get(`http://localhost:8080/students`);
+        setState({ loading: false, data: response.data || [] });
+    }
+
+    const [state, setState] = useState({ loading: true, data: [] });
+    const { loading, data } = state || {};
+
     useEffect(() => {
-        // async function getAll() {
-        //     const response = await axios.get(`http://localhost:8080/students`)
-        //         .then(res => res.data)
-        //     setStudent(response);.
-                fetch('http://localhost:8080/students')
-                .then(response => 
-                    response.json())
-                .then(data => {
-                    setStudent(data)});
-        //getAll()
-        
+        getAll();
     }, [])
-    
+
+     async function handleOnSubmit(e) {
+        e.preventDefault();
+        const data = {
+            name,
+            codeStudent : code,
+            address,
+            email
+        }
+        console.log(data)
+        setModal(false);
+        console.log("log")
+        await axios.post('http://localhost:8080/students', data);
+        await getAll();
+         
+    }
+    function handleOnAdd(){
+        setModal(true);
+    }
+    function handleCancel(){
+        setModal(false);
+    }
+
+
+    const [name, setName] = useState();
+    const [code, setCodeStudent] = useState();
+    const [address, setAddress] = useState();
+    const [email, setEmail] = useState();
+
+    const [modal, setModal] = useState(false);
 
     return (
         <div>
             <h1>Student list</h1>
             <p> </p>
-            <Button  color="primary" >Add</Button>
+            <Button color="primary" onClick={handleOnAdd} >Add</Button>
             <p> </p>
-            <Table bordered>
-                <thead>
-                    <tr>
-                        <td><b>Id</b></td>
-                        <td><b>Name</b></td>
-                        <td><b>CodeStudent</b></td>
-                        <td><b>Address</b></td>
-                        <td><b>Email</b></td>
-                        <td>
+            {!loading && (
+                <Table bordered>
+                    <thead>
+                        <tr>
+                            <td><b>Id</b></td>
+                            <td><b>Name</b></td>
+                            <td><b>CodeStudent</b></td>
+                            <td><b>Address</b></td>
+                            <td><b>Email</b></td>
+                            <td>
 
-                        </td>
-                    </tr>
-                </thead>
-                <tbody>
-                    {
-                        students && students.map(student => (
-                            <tr>
-                                <td>{student.id}</td>
-                                <td>{student.name}</td>
-                                <td>{student.codeStudent}</td>
-                                <td>{student.address}</td>
-                                <td>{student.email}</td>
-                                <td>
-                                    <Button color="info">Edit</Button>
-                                    <Button  onClick={() => {
-                                    deleteTask(student.id);
-                                    }} color="danger">Delete
+                            </td>
+                        </tr>
+                    </thead>
+                    <tbody> 
+                        {
+                            data && data.map((item, courseIndex) => (
+                                <tr key={courseIndex}>
+                                    <td>{item.id}</td>
+                                    <td>{item.name}</td>
+                                    <td>{item.codeStudent}</td>
+                                    <td>{item.address}</td>
+                                    <td>{item.email}</td>
+                                    <td>
+                                        <Button color="info" >Edit</Button>
+                                        <Button onClick={() => {
+
+                                            deleteStudent(item.id);
+                                        }} color="danger">Delete
                                     </Button>
-                                </td>
-                            </tr>
-                        ))
-                    }
-                </tbody>
+                                    </td>
+                                </tr>
+                            ))
+                        }
+                    </tbody>
 
-            </Table>
+                </Table>
+            )}
+
+
+            <div>
+
+                <Modal isOpen={modal} fade={true}   >
+                    <ModalHeader >Add Student</ModalHeader>
+                    <ModalBody>
+                        <div>
+                            <form id="formSubmit" onSubmit={handleOnSubmit} >
+                                <div>
+                                    <label for="name">Name:</label> <br />
+                                    <input type="text" id="name" name="name"
+                                        value={name} onChange={e => setName(e.target.value)} /> <br />
+                                </div>
+                                <div>
+                                    <label for="code">CodeStudent:</label><br />
+                                    <input type="text" id="code" name="code"
+                                        value={code} onChange={e => setCodeStudent(e.target.value)} /> <br />
+                                </div>
+                                <div>
+                                    <label for="address">Address:</label> <br />
+                                    <input type="text" id="address" name="address"
+                                        value={address} onChange={e => setAddress(e.target.value)} /> <br />
+                                </div>
+                                <div>
+                                    <label for="email">Email:</label> <br />
+                                    <input type="text" id="email" name="email" 
+                                    value={email}  onChange={e => setEmail(e.target.value)}  /> <br />
+                                </div>
+
+                            </form>
+                        </div>
+                    </ModalBody>
+                    <ModalFooter>
+                        <Button color="primary" type="submit"  form="formSubmit" >Save</Button>{' '}
+                        <Button color="secondary" onClick={handleCancel}>Cancel</Button>
+                    </ModalFooter>
+                </Modal>
+            </div>
         </div>
 
     )
