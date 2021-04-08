1da78a7798fc476b076d14ff8f47e652edbe2b6e
nmduc99 <65432847+nmduc99@users.noreply.github.com>
2021-04-08 20:16:22 +0700

    Update App.js
    update_8.4.21

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import StudentList from './components/student/StudentList';
 import CourseList from './components/course/CourseList';
 import React, { useState } from "react";
 
-import { HomeOutlined, SolutionOutlined, BarsOutlined } from '@ant-design/icons';
 import {
   // BrowserRouter as Router,
   //Switch,
@@ -13,44 +12,56 @@ import {
 } from "react-router-dom";
 import { Col } from 'reactstrap';
 import NormalLoginForm from './components/authentication/NormalLoginForm'
-import { Alert, Menu  } from 'antd';
+import { Alert, Menu } from 'antd';
 
-const { SubMenu } = Menu;
-const Index = () => <h1>Home</h1>;
 function App() {
 
-  const [authen, setAuthen] = useState(false);
+  const [authen, setAuthen] = useState(localStorage.getItem("authen") === "true");
   let history = useHistory();
 
   function Login(username, password) {
-    console.log(username)
     if (username === 'admin') {
       setAuthen(true)
+      localStorage.setItem("authen", true)
     }
   }
 
-
   const renderComponent = (authen) => {
+
     if (authen) {
+
       return (
         <React.Fragment>
+          <Menu
+            style={{ width: 256 }}
+            mode="inline"
+            onClick={handleClick}
+          >
+            {menuData.map((menuItem, menuIndex) => {
+              const { text, key } = menuItem;
+              return (
+                <Menu.Item key={key}>{text}</Menu.Item>
+              )
+            }
+            )}
+
+          </Menu>
           <Col sm="3">
             <Alert  //"Logged in successfully" 
-             message={
-              "Logged in successfully"
-             }
-             type="success"  closable />
+              message={
+                "Logged in successfully"
+              }
+              type="success" closable />
+       
           </Col>
-          <Route path="/home" exact component={Index} />
-          <Route path="/students/" exact component={StudentList} />
+          <Route path="/students/"  component={StudentList} />
           <Route path="/courses/" exact component={CourseList} />
         </React.Fragment>
       )
     } else {
       return (
-        <Route path="/" exact render={() => {
-          return !authen && <NormalLoginForm login={Login} />
-        }} />
+        <NormalLoginForm login={Login} />
+       
       );
     }
   }
@@ -60,39 +71,13 @@ function App() {
     history.push(e.key);
   }
 
+  const menuData = [
+    { key: "/students", text: "Student" },
+    { key: "/courses", text: "Courses" }
+  ]
+
   return (
     <div className="App">
-      {/* <Nav>
-          <NavItem>
-            <Link to="/home"><h4>Home </h4></Link>
-          </NavItem>
-          <NavItem>
-            <Link to="/students"><h4>Student </h4></Link>
-          </NavItem>
-          <NavItem>
-            <Link to="/courses"><h4>Course</h4></Link>
-          </NavItem>
-        </Nav>*/}
-
-      <Menu
-        style={{ width: 256 }}   
-        mode="vertical"
-        onClick={handleClick}        
-      >
-
-        <SubMenu key="sub1" icon={<HomeOutlined />} title="Home">
-          <Menu.Item key="/home">Home</Menu.Item>
-        </SubMenu>
-
-        <SubMenu key="sub2" icon={<SolutionOutlined />} title="Student">
-          <Menu.Item key="/students">Student List</Menu.Item>
-        </SubMenu>
-
-        <SubMenu key="sub3" icon={<BarsOutlined />} title="Course">
-          <Menu.Item key="/courses">Course List</Menu.Item>
-        </SubMenu>
-      </Menu>
-
       {renderComponent(authen)}
     </div>
   );
